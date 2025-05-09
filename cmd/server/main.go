db2042b11f2f79bc049d5b7959ca4e8bf05057a9
Dorri <sara.dorri@zoodfood.com>
2025-05-09 16:25:34 +0330

    Update main function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,25 @@
-package server
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"tiny-ledger/internal/handlers"
+	"tiny-ledger/internal/services"
+	"tiny-ledger/internal/store"
+)
+
+func main() {
+	ledgerStore := store.NewLedgerStore()
+	ledgerService := services.NewLedgerService(ledgerStore)
+	ledgerHandler := handlers.NewLedgerHandler(ledgerService)
+
+	r := mux.NewRouter()
+	ledgerHandler.RegisterRoutes(r)
+
+	log.Println("Server is running on port 8080")
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		return
+	}
+}
